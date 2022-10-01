7e9c82f9d183c0c6e106439c7e373170d771d1b0
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-09-30 17:31:37 -0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,5 @@
 -----------------For support, scripts, and more----------------
------------------ https://discord.gg/XJFNyMy3Bv ---------------
+--------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
 if Config.Framework == "ESX" then
         if Config.OldESX then

71f39077f0e100b860f61bca0112c04e93a0f71d
Mario <102430767+SpalenyBramborak@users.noreply.github.com>
2022-10-14 11:05:01 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,8 +11,6 @@ version '2.1.4'
 lua54 'yes'
 
 client_scripts {
-  '@es_extended/locale.lua',
-  'locales/*.lua',
   'client/**.lua'
 }
 
@@ -29,4 +27,8 @@ shared_scripts {
 dependencies {
   'xsound',
   'ox_lib'
+}
+
+files {
+  'locales/en.json'
 }
